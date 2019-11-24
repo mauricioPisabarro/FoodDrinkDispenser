e06466802684fbe24116fdaa464661644eb89744
Mauricio Pisabarro <mauripisabarro@gmail.com>
2019-11-24 12:28:47 -0300

    Remove unnecessary dependencies

--- food_drink_dispenser/management/commands/listen_events.py
+++ food_drink_dispenser/management/commands/listen_events.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from food_drink_dispenser.models import FoodLog, DrinkLog, FoodDispenseRequest, DrinkDispenseRequest
+from food_drink_dispenser.models import FoodLog, DrinkLog
 from django.db.models import signals
 
 class Command(BaseCommand):
