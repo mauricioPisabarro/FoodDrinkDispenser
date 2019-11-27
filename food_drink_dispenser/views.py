2cd96296a771dbbad1a24814d672b0ec86869957
Nicolás Eirin <nicoeirin@gmail.com>
2019-11-27 12:02:00 -0300

    Binding models

--- food_drink_dispenser/views.py
+++ food_drink_dispenser/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from food_drink_dispenser.models import DrinkLog
+from food_drink_dispenser.models import FoodLog
 
 # from polls.models import Sensor
 
 def index(request):
     template = loader.get_template('food_drink_dispenser/index.html')
     context = {
-
+        
     }
     return HttpResponse(template.render(context, request))
 
 def water_logs(request):
     template = loader.get_template('food_drink_dispenser/water_logs.html')
+    list_water = DrinkLog.objects.all()
     context = {
-
+        'list_water': list_water 
     }
     return HttpResponse(template.render(context, request))
 
 def food_logs(request):
     template = loader.get_template('food_drink_dispenser/food_logs.html')
+    list_food = FoodLog.objects.all()
     context = {
-
+        'list_food': list_food
     }
     return HttpResponse(template.render(context, request))
 
