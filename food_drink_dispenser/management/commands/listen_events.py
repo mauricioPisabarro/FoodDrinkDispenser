7a36bf1dc2b182d670b07e41c7eb976e0d17c323
Mauricio Pisabarro <mauripisabarro@gmail.com>
2019-11-27 11:33:51 -0300

    Update mqtt listener and fix encoding

--- food_drink_dispenser/management/commands/listen_events.py
+++ food_drink_dispenser/management/commands/listen_events.py
@@ -3,7 +3,7 @@ from food_drink_dispenser.models import FoodLog, DrinkLog
 from django.db.models import signals
 import paho.mqtt.client as mqtt
 
-IS_TESTING_ON_LINUX = True
+IS_TESTING_ON_PLAIN_LINUX = True
 FOOD_TOPIC_SUBSCRIPTION = 'foodOutTopic'
 WATER_TOPIC_SUBSCRIPTION = 'waterOutTopic'
 
@@ -11,16 +11,13 @@ FOOD_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION = 'foodConfirmationOutTopic'
 WATER_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION = 'waterConfirmationOutTopic'
 
 class Command(BaseCommand):
-    def miFuncion(self):
-        print('MI FUNCION')
-
     def handle(self, *args, **options):
         client = mqtt.Client(client_id='mqtt-test')
         client.on_connect = on_connect
         client.on_message = on_message
 
-        if IS_TESTING_ON_LINUX:
-            client.connect('mqtt.eclipse.org', 1883, 60)
+        if IS_TESTING_ON_PLAIN_LINUX:
+            client.connect('127.0.0.1', 1883, 60)
         else:
             client.username_pw_set('domotica', 'natdanv07')
             client.connect('127.0.0.1', port=1883)
@@ -40,17 +37,17 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     topic = msg.topic
-    data = msg.payload
+    data = msg.payload.decode('utf-8')
 
-    if str(topic) == FOOD_TOPIC_SUBSCRIPTION:
+    if topic == FOOD_TOPIC_SUBSCRIPTION:
         print(FOOD_TOPIC_SUBSCRIPTION)
-        print(str(data))
-    elif str(topic) == WATER_TOPIC_SUBSCRIPTION:
+        print(data)
+    elif topic == WATER_TOPIC_SUBSCRIPTION:
         print(WATER_TOPIC_SUBSCRIPTION)
-        print(str(data))
-    elif str(topic) == FOOD_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION:
+        print(data)
+    elif topic == FOOD_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION:
         print(FOOD_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION)
-        print(str(data))
-    elif str(topic) == WATER_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION:
+        print(data)
+    elif topic == WATER_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION:
         print(WATER_DISPENSE_CONFIRMATION_TOPIC_SUBSCRIPTION)
-        print(str(data))
+        print(data)
