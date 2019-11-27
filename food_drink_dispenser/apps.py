6cf547f665b5033ac23713ed002315e8889693da
Mauricio Pisabarro <mauripisabarro@gmail.com>
2019-11-27 10:52:29 -0300

    Remove unused code

--- food_drink_dispenser/apps.py
+++ food_drink_dispenser/apps.py
@@ -1,12 +1,4 @@
 from django.apps import AppConfig
-from django.db.models.signals.pre_save import pre_save
 
 class FoodDrinkDispenserConfig(AppConfig):
     name = 'food_drink_dispenser'
-
-    def ready(self):
-        from .models import FoodDispenseRequest, DrinkDispenseRequest
-        from .signals import pre_save_dispense_food_handler, pre_save_dispense_drink_handler
-
-        pre_save.connect(pre_save_dispense_food_handler, sender=FoodDispenseRequest)
-        pre_save.connect(pre_save_dispense_drink_handler, sender=DrinkDispenseRequest)
