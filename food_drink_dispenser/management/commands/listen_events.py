47905fc8efbed1bdee4f5144937f03555150c0f2
Mauricio Pisabarro <mauripisabarro@gmail.com>
2019-11-27 12:15:21 -0300

    Register post_save signals

--- food_drink_dispenser/management/commands/listen_events.py
+++ food_drink_dispenser/management/commands/listen_events.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 
 
 def on_connect(client, userdata, flags, rc):
-    print('Connected with result code: ' + str(rc))
+    print('LISTENER: Connected with result code: ' + str(rc))
 
     client.subscribe(FOOD_TOPIC_SUBSCRIPTION)
     client.subscribe(WATER_TOPIC_SUBSCRIPTION)
