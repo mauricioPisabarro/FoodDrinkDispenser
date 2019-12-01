077c4b6173e08e2065a525158fe4130589e63c93
Mauricio Pisabarro <mauripisabarro@gmail.com>
2019-11-30 23:41:57 -0300

    Create food/drink request views

--- food_drink_dispenser/urls.py
+++ food_drink_dispenser/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('water_logs/', views.water_logs, name='water_logs'),
     path('food_logs/', views.food_logs, name='food_logs'),
+    path('water_requests/', views.water_requests, name='water_requests'),
+    path('food_requests/', views.food_requests, name='food_requests'),
     path('dispense_food/', views.dispense_food, name='dispense_food'),
     path('dispense_water/', views.dispense_water, name='dispense_water'),
 ]
--- food_drink_dispenser/views.py
+++ food_drink_dispenser/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-from food_drink_dispenser.models import DrinkLog, FoodLog, FoodDispenseRequest, DrinkDispenseRequest
+from food_drink_dispenser.models import DrinkLog, FoodLog, FoodDispenseRequest, DrinkDispenseRequest, RequestStatuses
 from django.views.decorators.csrf import csrf_exempt
 
-# from polls.models import Sensor
 
 def index(request):
     template = loader.get_template('food_drink_dispenser/index.html')
@@ -15,33 +14,50 @@ def index(request):
 
 def water_logs(request):
     template = loader.get_template('food_drink_dispenser/water_logs.html')
-    list_water = DrinkLog.objects.all()
-    context = {
-        'list_water': list_water
-    }   
+    waterLogList = DrinkLog.objects.all().order_by('-date')
+    context = { 'waterLogList': waterLogList }   
 
     return HttpResponse(template.render(context, request))
 
 
 def food_logs(request):
     template = loader.get_template('food_drink_dispenser/food_logs.html')
-    list_food = FoodLog.objects.all()
-    context = {
-        'list_food': list_food
-    }
+    foodLogList = FoodLog.objects.all().order_by('-date')
+    context = { 'foodLogList': foodLogList }
 
     return HttpResponse(template.render(context, request))
+
+
+def water_requests(request):
+    template = loader.get_template('food_drink_dispenser/water_requests.html')
+    waterRequestsList = DrinkDispenseRequest.objects.all().order_by('-date')
+    
+    context = { 'waterRequestsList': map(beautifyObject, waterRequestsList) }
+
+    return HttpResponse(template.render(context, request))
+
+
+def food_requests(request):
+    template = loader.get_template('food_drink_dispenser/food_requests.html')
+    foodRequestsList = FoodDispenseRequest.objects.all().order_by('-date')
+    context = { 'foodRequestsList': map(beautifyObject, foodRequestsList) }
+
+    return HttpResponse(template.render(context, request))
+
 
 @csrf_exempt
 def dispense_food(request):
     if request.method == 'POST':
         waterRequest = FoodDispenseRequest.objects.create()
         waterRequest.save()
+        
+        return redirect('food_requests') 
     
     template = loader.get_template('food_drink_dispenser/dispense_food.html')
     context = { }
 
     return HttpResponse(template.render(context, request))
+
 
 @csrf_exempt
 def dispense_water(request):
@@ -49,7 +65,27 @@ def dispense_water(request):
         waterRequest = DrinkDispenseRequest.objects.create()
         waterRequest.save()
 
+        return redirect('water_requests')
+
     template = loader.get_template('food_drink_dispenser/dispense_water.html')
     context = { }
 
     return HttpResponse(template.render(context, request))
+
+
+def beautifyObject(obj):
+    if obj.status == RequestStatuses.FAILED:
+        return {
+            'date': obj.date,
+            'status': 'FAILED',
+        }
+    elif obj.status == RequestStatuses.SUCCESSFUL:
+        return {
+            'date': obj.date,
+            'status': 'SUCCESSFUL',
+        }
+
+    return {
+        'date': obj.date,
+        'status': 'PENDING',
+    }
